44c5a616c7b829295cdf181fe500d48d8ae3eff8
Luciane da Silva <lucianes2009@hotmail.com>
2016-07-06 10:38:33 -0300

    Ajustes finais

--- src/test/java/br/edu/ifrs/canoas/lds/SpringStarterApplicationTests.java
+++ src/test/java/br/edu/ifrs/canoas/lds/SpringStarterApplicationTests.java
@@ -2,11 +2,11 @@ package br.edu.ifrs.canoas.lds;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class SpringStarterApplicationTests {
 
 	@Test
